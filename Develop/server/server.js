001e983f1148b0a62338c68ede63d2dfe62a80ec
Za Matheson <zamatheson@Zas-MacBook-Air.lan>
2024-02-12 20:56:00 -0600

    server.js and added schemas files

--- Develop/server/server.js
+++ Develop/server/server.js
@@ -1,14 +1,34 @@
 const express = require('express');
 const path = require('path');
+
+// import ApolloServer
+const { ApolloServer } = require('apollo-server-express');
+// import our typeDefs and resolvers
+const { typeDefs, resolvers } = require('./schemas');
+const { authMiddleware } = require('./utils/auth');
+
 const db = require('./config/connection');
-const routes = require('./routes');
 
 const app = express();
 const PORT = process.env.PORT || 3001;
 
+// import routes
+const server = new ApolloServer({
+  typeDefs,
+  resolvers,
+  context: authMiddleware,
+});
+
+// integrate the apollo server with the express application as middleware
+server.applyMiddleware({ app });
+
+// middleware
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
+const __dirname = path.dirname('');
+const buildPath = path.join(__dirname, '../client/build');
+app.use(express.static(buildPath));
 // if we're in production, serve client/build as static assets
 if (process.env.NODE_ENV === 'production') {
   app.use(express.static(path.join(__dirname, '../client/build')));
@@ -19,3 +39,5 @@ app.use(routes);
 db.once('open', () => {
   app.listen(PORT, () => console.log(`🌍 Now listening on localhost:${PORT}`));
 });
+
+startApolloServer(typeDefs, resolvers);
