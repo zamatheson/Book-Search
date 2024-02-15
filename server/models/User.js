45468d59962b5a1990da76c0fec3cc6976996231
Za Matheson <zamatheson@Zas-MacBook-Air.lan>
2024-02-14 21:36:08 -0600

    fixed bcryptjs

--- server/models/User.js
+++ server/models/User.js
@@ -1,5 +1,5 @@
 const { Schema, model } = require('mongoose');
-const bcrypt = require('bcrypt');
+const bcryptjs = require('bcryptjs');
 
 // import schema from Book.js
 const bookSchema = require('./Book');
@@ -36,7 +36,7 @@ const userSchema = new Schema(
 userSchema.pre('save', async function (next) {
   if (this.isNew || this.isModified('password')) {
     const saltRounds = 10;
-    this.password = await bcrypt.hash(this.password, saltRounds);
+    this.password = await bcryptjs.hash(this.password, saltRounds);
   }
 
   next();
@@ -44,7 +44,7 @@ userSchema.pre('save', async function (next) {
 
 // custom method to compare and validate password for logging in
 userSchema.methods.isCorrectPassword = async function (password) {
-  return bcrypt.compare(password, this.password);
+  return bcryptjs.compare(password, this.password);
 };
 
 // when we query a user, we'll also get another field called `bookCount` with the number of saved books we have
